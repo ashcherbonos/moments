652d0fa762ce4fdbc0c54361fc9714aa3e2e6916
OleksandrShcherbonos <ashcherbonos@gmail.com>
2023-01-06 15:48:32 +0100

    front feat: Project setup

---
+++ front/src/App.vue
@@ -0,0 +1,32 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  data() {
+    return {
+      todoData: null,
+    };
+  },
+  methods: {
+    async fetchData() {
+      this.todoData = null;
+      const res = await fetch("http://localhost:8000/api/galleries");
+      this.todoData = await res.json();
+    },
+  },
+  mounted() {
+    this.fetchData();
+  },
+});
+</script>
+
+<template>
+  <main>
+    <p v-if="!todoData">Loading...</p>
+    <pre v-else>{{ todoData }}</pre>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,16 @@
+import cors from 'cors';
 import dotenv from 'dotenv';
 import express, { Express } from 'express';
 import { connect } from 'mongoose';
+import { crossConfig } from './config/cross.config';
 import { dbConfig } from './config/db.config';
 import { galleriesRoute } from './routes/galleries.routes';
 
 dotenv.config();
 
 const app: Express = express();
+
+app.use(cors(crossConfig));
 
 app.use('/api/galleries', galleriesRoute);
 
